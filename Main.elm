43ef2c3a0bd6f2cb66bbf1ae3b1c27a4ab0c4542
yuffiy <yfhj1990@hotmail.com>
2016-05-26 18:07:18 +0800

    update.

--- Main.elm
+++ Main.elm
@@ -1,84 +1,157 @@
 module Main exposing (..)
 
-import Html exposing (..)
+{-|
+
+
+
+-}
+
+import Html exposing (text)
 import Regex exposing (..)
 import Debug exposing (log)
 import String
 
+
+type alias Matcher a = List (Maybe String) -> Block a
+
+type alias Reverser a = Block a -> String
+  
+
 type Block a
-  = Header a
-  | Paragraph
+  = Paragraph a
+  | Header a
 
 
-flatmap : Maybe (Maybe a) -> Maybe a
-flatmap maybe =
-  let
-    _ =
-      log "maybe" maybe
-  in
-    case maybe of
-      Just (Just a) ->
-        Just a
-      _ ->
-        Nothing
+matchHeader : Matcher (Maybe Int, Maybe String)
+matchHeader header =
+  case header of
+    num::content::[] ->
+      Header (Maybe.map String.length num, content)
+    _ ->
+      Header (Nothing, Nothing)
 
 
-makeHeader : List (Maybe String) -> Block (Maybe Int, Maybe String)
-makeHeader header =
-  let
-    (num, content) =
-      case header of
-        num::content::xs -> (num, content)
-        _ -> (Nothing, Nothing)
-  in
-    Header (Maybe.map String.length num, content)
-
-reverseHeader : Block (Maybe Int, Maybe String) -> String
+reverseHeader : Reverser (Maybe Int, Maybe String)
 reverseHeader header =
   case header of
-    Header (num, content) ->
-      "h2 aa"
+    Header (Just num, Just content) ->
+      viewHeader num content
     _ ->
       ""
 
 
-reHeader : Regex
-reHeader =
-  regex "^(\\*+)\\s*(.*)"
+replaceBlock : Matcher a -> Reverser a -> Match -> String
+replaceBlock matcher reverser { submatches } =
+  submatches
+    |> matcher
+    |> reverser
 
-defaultHeader : Int
-defaultHeader =
-  1
 
-defaultContent : String
-defaultContent =
-  ""
 
-    
-
-replaceHeader : Match -> String
-replaceHeader { match, submatches, index } =
-  let
-    _ =
-      log "match" match
-    _ =
-      log "submatches" (reverseHeader <| makeHeader submatches) |> toString
-    _ =
-      log "index" index |> toString
-
-  in
-    match
-
-reList : Regex
-reList =
-  regex ""
+replaceHeader =
+  replaceBlock matchHeader reverseHeader
 
 
 test : String
 test = """
-* 123
+### 123
+
+haaaahaaa
+
+## header 2
+axx
+
+456
 """
 
 
+viewHeader : Int -> String -> String
+viewHeader num content =
+  let
+    n =
+      toString num
+  in
+    "<h" ++ n ++ ">" ++ content ++ "</h" ++ n ++ ">"
+
+defaultBlock : String -> String
+defaultBlock content =
+  "<p>" ++ content ++ "</p>"
+
+
+reHeader : Regex
+reHeader =
+  regex "(#+)\\s*(.*)"
+
+
+type alias Matching a =
+  { flag : a -> Block a
+  , matcher : Match -> String
+  , regexp : Regex
+  , result : Bool
+  , match : String
+  }
+
+headerMatching : Matching a
+headerMatching =
+  { flag = Header
+  , matcher = replaceBlock matchHeader reverseHeader
+  , regexp = reHeader
+  , result = False
+  , match = ""
+  }
+
+        
+matchers : List (Matching a)
+matchers =
+  [ headerMatching
+  ]
+          
+
+splitBlocks : String -> List String
+splitBlocks =
+  Regex.split Regex.All <| regex "\\n{2}"
+       
+
+convertBlock : String -> String       
+convertBlock str =
+  let
+    matching m =
+      { m |
+        result = Regex.contains m.regexp str
+      , match = str
+      }
+    
+    matched =
+      List.map matching matchers
+
+    filted =
+      List.filter (\{ result } -> result) matched
+
+    result =
+      List.head filted
+
+    replacer =
+      Regex.replace Regex.All
+
+    default =
+      defaultBlock str
+
+  in
+    case result of
+      Just { flag, matcher, regexp, match } ->
+        case flag match of
+          Header match ->
+            replacer regexp matcher match
+          _ ->
+            default
+      Nothing ->
+        default
+
+
 main =
-  toString >> text <| replace All reHeader replaceHeader "** 123asdf"
+  test
+    |> String.trim
+    |> splitBlocks
+    |> List.map convertBlock
+    |> toString
+    |> text
